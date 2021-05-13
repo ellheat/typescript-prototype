a9592f2ad085684b4eb8a4a274b6612fef63309d
Patryk Poleszczuk <patryk.poleszczuk@gmail.com>
2021-05-13 21:58:24 +0200

    Improve pagination

--- src/hooks/useRepositories/useRepositories.tsx
+++ src/hooks/useRepositories/useRepositories.tsx
@@ -1,25 +1,21 @@
-import { useState, Dispatch, SetStateAction } from 'react';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
 import { URL, LIMIT } from './useRepositories.constants';
 
-type FetchRepositoriesType = (query: string, page: number, setPagesCount: Dispatch<SetStateAction<number>>) => void
+type FetchRepositoriesType = Promise<{ items: [], total_count: number, incomplete_results: boolean }>
 type UseRepositoriesResult = { status: string, pagesCount: number, repositories: [], error: unknown }
 
 
-const fetchRepositories: FetchRepositoriesType = async (query, page= 1, setPagesCount) => {
+const fetchRepositories: (query: string, page?: number) => FetchRepositoriesType = async (query, page= 1) => {
   const { data } = await axios.get(`${URL}?q=${query}&page=${page}&per_page=${LIMIT}`);
-  setPagesCount(Math.ceil(data.total_count / LIMIT));
-  return data.items;
+  return data;
 };
 
 export const useRepositories: (query: string, page: number) => UseRepositoriesResult = (query, page) => {
-  const [pagesCount, setPagesCount] = useState<number>(1);
-
   const { status, data, error } = useQuery(
     ['repositories', query, page],
-    () => fetchRepositories(query, page, setPagesCount),
+    () => fetchRepositories(query, page),
     {
       enabled: query !== '',
       retry: false,
@@ -27,10 +23,12 @@ export const useRepositories: (query: string, page: number) => UseRepositoriesRe
     }
   );
 
+  const totalCount = data?.total_count || 1;
+
   return {
     status,
-    pagesCount,
-    repositories: data || [],
+    pagesCount: totalCount <= LIMIT ? 1 : Math.ceil(totalCount / LIMIT),
+    repositories: data?.items || [],
     error,
   };
 }
